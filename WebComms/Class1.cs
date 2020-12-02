03b93f1141ea1d8f54d05b55476b6468e9f3b2c5
Qasim Chaudhry <qasim@ayudasystems.com>
2020-12-02 17:41:01 +1100

    Remove unused classes

--- WebComms/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WebComms
-{
-    public class Class1
-    {
-    }
-}
--- WooliesX.DependencyInjection/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WooliesX.DependencyInjection
-{
-    public class Class1
-    {
-    }
-}
